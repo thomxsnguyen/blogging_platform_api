3a9701e5cdb54366419ecad88be44862233a38b1
Thomas <thomasn8255@gmail.com>
2025-04-04 21:55:14 -0700

    test code

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(100), unique=True)
+    date_joined = db.Column(db.Date, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
 
 @app.route('/')
 def home():
@@ -13,4 +30,6 @@ def create_post():
   return {"status": "recieved"}, 200
 
 if __name__ == "__main__":
+  with app.app_context():
+     db.create_all()
   app.run(debug=True, port=5000)
