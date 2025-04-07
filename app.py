8ea3dffb70c73df9da8a73b6ce65a46223d1564d
Thomas <thomasn8255@gmail.com>
2025-04-07 00:03:35 -0700

    created create method with tag attribute

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import ARRAY
 from datetime import datetime
 import os
 
@@ -7,19 +8,18 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'db.sqlite3')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(100), unique=True)
-    date_joined = db.Column(db.Date, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<User {self.name}>'
+    title = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.String(150), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 @app.route('/')
 def home():
@@ -28,10 +28,20 @@ def home():
 @app.route('/create', methods=['POST'])
 def create_post():
   data = request.json
-  print(data)
+  title = data.get('title')
+  content = data.get('content')
+  category = data.get('category')
+  tags = data.get('tags')
+  
+  user = User(title=title, content=content, category=category)
+
+  db.session.add(user)
+  db.session.commit()
   return {"status": "recieved"}, 200
+
 
 if __name__ == "__main__":
   with app.app_context():
-     db.create_all()
+    db.create_all()
+     
   app.run(debug=True, port=5000)
