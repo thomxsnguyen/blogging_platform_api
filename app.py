c8b6934dda3485d64765c94301728c50822645d0
Thomas <thomasn8255@gmail.com>
2025-04-07 13:22:44 -0700

    implemented tags will fix "\" later

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import ARRAY
 from datetime import datetime
+import json
 import os
 
 #flask shell
+
+# curl -X POST http://localhost:5000/create \
+#   -H "Content-Type: application/json" \
+#   -d '{
+#     "title": "Tagged Post",
+#     "content": "This one has tags stored in JSON.",
+#     "category": "Tutorial",
+#     "tags": ["flask", "api", "sqlite"]
+# }'
 
 app = Flask(__name__)
 
@@ -18,8 +28,13 @@ class User(db.Model):
     title = db.Column(db.String(50), nullable=False)
     content = db.Column(db.String(150), nullable=False)
     category = db.Column(db.String(50), nullable=False)
+    tags = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def set_tags(self, tag_list):
+      self.tags = json.dumps(tag_list)
+
 
 @app.route('/')
 def home():
@@ -34,10 +49,23 @@ def create_post():
   tags = data.get('tags')
   
   user = User(title=title, content=content, category=category)
+  temp = []
+  for x in tags:
+    temp.append(x)
+  user.set_tags(temp)
 
   db.session.add(user)
   db.session.commit()
   return {"status": "recieved"}, 200
+
+@app.route('/view', methods=['GET'])
+def view_post():
+  user_id = request.args.get("id")
+  if not user_id:
+    return {"error": "Missing 'id' parameter"}, 400
+  user = User.query.get(user_id)
+  print(user)
+  return {"title": user.title, "content": user.content, "category": user.category, "tags": user.tags, "createdAt":user.created_at, "updatedAt": user.updated_at}, 200
 
 
 if __name__ == "__main__":
